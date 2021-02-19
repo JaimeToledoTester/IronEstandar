7abbbf583b4eb147fff6cf48b55913015cd1215f
Jaime Toledo <jaimetoledo@recursoconfiable.com>
2021-02-19 10:13:02 -0600

    Guardando cambios de JaimeMejoras con singleton

--- src/test/java/Test/Login.java
+++ src/test/java/Test/Login.java
@@ -1,16 +1,6 @@
 package Test;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
-<<<<<<< HEAD
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-
-import static org.testng.Assert.assertTrue;
-import org.openqa.selenium.*;
-import org.testng.*;
-=======
->>>>>>> JaimeMejoras
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterTest;
@@ -19,43 +9,14 @@ import POM.Base;
 import POM.CLogin;
 
 public class Login {
-<<<<<<< HEAD
-	CapturarPantalla c = new CapturarPantalla();// objeto para otra clase
-	Reporte rep = Reporte.getInstance();
-	Parametros param = Parametros.getInstance();	
-	ExtentTest test;
-	ExtentReports extent;
-	private WebDriver driver;
-	CLogin clogin;
-	MediaEntityModelProvider evidencia;
-    String Page;
-    By asser=By.xpath("//*[contains(@href,'LogOut')]"); 
-
-
-	@BeforeClass
-	public void beforeClass()  {
-		clogin = new CLogin(driver);
-		driver = clogin.chrome();
-		extent=rep.addreporte();
-	} 
-	
-	@Test(dataProvider = "Usuarios", dataProviderClass = DataProvider.class)
-	public void Loginm(String empresa,String usuario,String contraseña, String url){
-	try {	
-    test=extent.createTest("CP: Validar Login de "+empresa);
-	clogin.login(extent,test,usuario, contraseña, url);
-	clogin.wait(asser);
-	assertTrue(clogin.isDisplayed(asser));
-=======
 	Base b=Base.getInstance();
 	CLogin clog=CLogin.getInstance();	
 	String Page;	 
 	 
 	@Test(dataProvider = "Usuarios", dataProviderClass = Data_Provider.class)
-	public void login(String empresa,String usuario,String contraseña, String url){
+	public void login(String empresa,String usuario,String contraseï¿½a, String url){
 	try {	 
-	clog.login(usuario, contraseña, url, empresa);
->>>>>>> JaimeMejoras
+	clog.login(usuario, contraseï¿½a, url, empresa);
 	Page= new Object(){}.getClass().getEnclosingMethod().getName();
 	}catch(Exception e) {System.out.println("Error en TestLogin, Metodo login "+e);}
 	}
--- src/test/java/Test/Planeacion_Txt.java
+++ src/test/java/Test/Planeacion_Txt.java
@@ -4,38 +4,6 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-<<<<<<< HEAD
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.MediaEntityModelProvider;
-import Adicional.CapturarPantalla;
-import Adicional.Reporte;
-import POM.CPlanTxt;
-
-public class Planeacion_Txt {
-
-	CapturarPantalla c = new CapturarPantalla();// objeto para otra clase
-	Reporte rep = Reporte.getInstance();	
-	ExtentTest test;
-	ExtentReports extent;
-	private WebDriver driver;
-	CPlanTxt cplan;
-	MediaEntityModelProvider evidencia;
-    String Page;
-
-	@BeforeClass
-	public void beforeClass()  {
-		cplan = new CPlaneacionTxt(driver);	
-		driver=cplan.chrome();
-		extent=rep.addreporte();
-	} 
-
-	@Test(dataProvider = "UsersandTxt", dataProviderClass = DataProvider.class)
-	public void Loginm(String empresa,String usuario, String contraseña, String ruta, String url){
-=======
 import org.testng.annotations.Test;
 import Adicional.Data_Provider;
 import POM.Base;
@@ -48,12 +16,11 @@ public class Planeacion_Txt {
     String Page;
 
 	@Test(dataProvider = "UsersandTxt", dataProviderClass = Data_Provider.class)
-	public void CargaPlan(String empresa,String usuario, String contraseña, String rutatxt, String url){
->>>>>>> JaimeMejoras
+	public void CargaPlan(String empresa,String usuario, String contraseï¿½a, String rutatxt, String url){
 	try {
-    cplan.CargarTxt(usuario, contraseña, url, rutatxt,empresa);	
+    cplan.CargarTxt(usuario, contraseï¿½a, url, rutatxt,empresa);	
 	Page= new Object(){}.getClass().getEnclosingMethod().getName();
-	}catch(Exception e) {System.out.println("Error en Test Cargar Planeación, Metodo CargaPlan: "+e);}
+	}catch(Exception e) {System.out.println("Error en Test Cargar Planeaciï¿½n, Metodo CargaPlan: "+e);}
 	}
 
 	@AfterMethod
